b902be5adf5fd4014eca09ba68660a65028e2e1e
gaurang.omar <gaurang.omar@geminisolutions.in>
2018-02-03 13:33:00 +0530

    weightsort changes

--- src/com/codewar/fivekyu/WeightSort.java
+++ src/com/codewar/fivekyu/WeightSort.java
@@ -59,25 +59,26 @@ import java.util.Set;
 public class WeightSort {
 	public static void main(String[] args) {
 
-		System.out.println(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+		System.out.println(orderWeight("11 11 2000 10003 22 123 1234000 44444444 9999"));
 	}
 
 	public static String orderWeight(String strng) {
-
+		if (null == strng || strng.isEmpty())
+			return strng;
 		String arr[] = strng.trim().split("\\s+");
 		String s = "";
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		Map<Integer, Integer> mapForDup = new HashMap<Integer, Integer>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		Map<String, Integer> mapForDup = new HashMap<String, Integer>();
 		for (String str : arr) {
-			int key = Integer.parseInt(str);
-			int value = String.valueOf(key).chars().map(Character::getNumericValue).sum();
+			String key = str;
+			int value = key.chars().map(Character::getNumericValue).sum();
 			if (!map.containsKey(key))
 				map.put(key, value);
 			else
 				mapForDup.put(key, mapForDup.containsKey(key) ? mapForDup.get(key) + 1 : 1);
 		}
-		List<Entry<Integer, Integer>> l = sortByValue(map);
-		for (Entry<Integer, Integer> object : l) {
+		List<Entry<String, Integer>> l = sortByValue(map);
+		for (Entry<String, Integer> object : l) {
 			if (mapForDup.containsKey(object.getKey())) {
 				for (int i = 0; i < mapForDup.get(object.getKey()); i++) {
 					s += object.getKey() + " ";
@@ -89,14 +90,14 @@ public class WeightSort {
 	}
 
 	private static List sortByValue(Map map) {
-		Set<Entry<Integer, Integer>> set = map.entrySet();
-		List<Entry<Integer, Integer>> list = new ArrayList<Entry<Integer, Integer>>(set);
-		Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-			public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+		Set<Entry<String, Integer>> set = map.entrySet();
+		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
 				if (o1.getValue() != o2.getValue())
 					return (o1.getValue()).compareTo(o2.getValue());
 				else
-					return (String.valueOf(o1.getKey())).compareTo(String.valueOf(o2.getKey()));
+					return (o1.getKey()).compareTo(o2.getKey());
 
 			}
 		});
